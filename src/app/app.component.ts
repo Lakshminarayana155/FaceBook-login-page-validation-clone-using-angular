3f7bef3e2297fd4d932a39d333a4a2f999cae3ab
[LakshmiNarayana M] <lakshminarayanamanchuri@gmail.com>
2023-04-11 15:43:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fbloginvalidation';
+
+  loginuserform = new FormGroup({
+    username:new FormControl(null,[Validators.required,Validators.email]),
+    userpass: new FormControl(null,[Validators.required,Validators.minLength(6)])
+  })
+  userlogin(){
+    console.log(this.loginuserform.value)
+  }
 }
